98bd3f3f5e957396858959d187da437169c15683
Cyril de Wit <cyrilwit@gmail.com>
2020-11-23 10:42:29 +0100

    chore: move backend appication to web application

--- web-application/routes/api.php
+++ web-application/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 
+// Bijv. http://api.projecttrash.nl/v1/........
+
 Route::prefix('v1')->group(function () {
     Route::prefix('products')->group(function () {
         Route::get('/', [ProductController::class, 'index']);
