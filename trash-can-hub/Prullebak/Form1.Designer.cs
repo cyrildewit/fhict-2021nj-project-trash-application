948088b0d2a66d6b0d22cb5e78558a04623732d7
Vegguid <vegguid@gmail.com>
2020-12-01 14:22:36 +0100

    chore: add code for trash can hub

---
+++ trash-can-hub/Prullebak/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Prullebak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Connect = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 338);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(68, 85);
+            this.Connect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(70, 24);
+            this.Connect.TabIndex = 1;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(352, 89);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(92, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(352, 197);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(349, 236);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Connect);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ trash-can-hub/Prullebak/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.IO.Ports;
+
+namespace Prullebak
+{
+    public partial class Form1 : Form
+    {
+        bool connected = false;
+        String[] ports;
+        SerialPort port;
+        string barcode = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            getAvailableComPorts();
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+                Console.WriteLine(port);
+                if (ports[0] != null)
+                {
+                    comboBox1.SelectedItem = ports[0];
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            //{"De externe server heeft een fout geretourneerd: (404) Niet gevonden."}
+            if (e.KeyCode == Keys.Enter && connected == true)
+            {
+
+                barcode = textBox1.Text;
+                //string url = "http://10.0.0.8/api/v1/products/findByBarcode/" + textBox1.Text;
+                string url = "http://8ae60850091d.ngrok.io/api/v1/products/findByBarcode/" + textBox1.Text;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@url);
+                request.Headers.Add("X-TrashCan-UUID", "Basic ashAHasd87asdHasdas");
+                HttpWebResponse response;
+                try
+                {
+                    response = (HttpWebResponse)request.GetResponse();
+                    string content = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+                    JsonDocument doc = JsonDocument.Parse(content);
+                    JsonElement root = doc.RootElement;
+
+                    string andwoord = Convert.ToString(root.GetProperty("data").GetProperty("information"));
+                    port.Write("#1" + andwoord + "\n");
+                    Console.WriteLine(andwoord);
+
+                    andwoord = Convert.ToString(root.GetProperty("data").GetProperty("seperation_tray"));
+                    port.Write("#" + andwoord + "\n");
+                    Console.WriteLine(andwoord);
+                }
+                catch
+                {
+                    port.Write("#1" + "Error niet in   database\n");
+                    port.Write("#" + "rest" + "\n");
+                    response = null;
+                }
+
+
+                textBox1.Text = "";
+            }
+            if (e.KeyCode == Keys.Enter && connected == false)
+            {
+
+            }
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            if (connected)
+            {
+                Disconnect();
+                Connect.Text = "Connect";
+            }
+            else
+            {
+                GoConnect();
+                Connect.Text = "Disconnect";
+            }
+        }
+        void getAvailableComPorts()
+        {
+            ports = SerialPort.GetPortNames();
+        }
+
+        private void GoConnect()
+        {
+            connected = true;
+            string selectedPort = comboBox1.GetItemText(comboBox1.SelectedItem);
+            port = new SerialPort(selectedPort, 9600, Parity.None, 8, StopBits.One);
+            port.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            port.Open();
+
+        }
+
+        private void Disconnect()
+        {
+            connected = false;
+            port.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = port.ReadLine();
+
+        }
+
+
+        private void DataReceivedHandler(
+                        object sender,
+                        SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            Console.WriteLine(indata);
+            //string url = "http://10.0.0.8/api/v1/users/findByNFC/" +  indata + "/discarded-waste-records?barcode=" + barcode;
+            string url = "http://8ae60850091d.ngrok.io/api/v1/users/findByNFC/" + indata + "/discarded-waste-records?barcode=" + barcode;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@url);
+            request.Headers.Add("X-TrashCan-UUID", "6697746d-2d47-4ebc-9da8-b7c714800319");
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            HttpWebResponse response;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch
+            {
+                Console.WriteLine("error in geld toewijzen");
+            }
+        }
+    }
+}
