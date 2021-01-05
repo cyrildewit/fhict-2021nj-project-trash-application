18e3d6d3ce42af7b07b830641d775615dda3ec9c
Cyril de Wit <cyrilwit@gmail.com>
2021-01-05 09:40:01 +0100

    refactor: remove stacked themes

--- mobile-app/lib/main.dart
+++ mobile-app/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:stacked_themes/stacked_themes.dart';
 
 import 'package:project_trash/main_app.dart';
 import 'package:project_trash/app/locator.dart';
 
 Future main() async {
-  await ThemeManager.initialise();
   setupLocator();
   runApp(ProjectTrashApp());
 }
--- mobile-app/lib/main_app.dart
+++ mobile-app/lib/main_app.dart
@@ -1,25 +1,21 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart' hide Router;
 import 'package:stacked_services/stacked_services.dart';
-import 'package:stacked_themes/stacked_themes.dart';
 
 import 'package:project_trash/app/locator.dart';
 import 'package:project_trash/app/router.gr.dart';
-import 'package:project_trash/app/theme_setup.dart';
 
 class ProjectTrashApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ThemeBuilder(
-      themes: getThemes(),
-      builder: (context, regularTheme, darkTheme, themeMode) => MaterialApp(
-        title: 'Project TRASH',
-        debugShowCheckedModeBanner: false,
-        theme: regularTheme,
-        darkTheme: darkTheme,
-        themeMode: themeMode,
-        onGenerateRoute: Router(),
-        navigatorKey: locator<NavigationService>().navigatorKey,
-      ),
+    return MaterialApp(
+      title: 'Project TRASH',
+      debugShowCheckedModeBanner: false,
+      theme: FlexColorScheme.light(
+              colors: FlexColor.schemes[FlexScheme.green].light)
+          .toTheme,
+      onGenerateRoute: Router(),
+      navigatorKey: locator<NavigationService>().navigatorKey,
     );
   }
 }
