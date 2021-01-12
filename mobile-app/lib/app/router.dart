afd670287e7e4c4c4a849e2c01a81e29e2945ca7
Cyril de Wit <cyrilwit@gmail.com>
2021-01-12 09:37:48 +0100

    feat: guard routes with auth guard

--- mobile-app/lib/app/router.dart
+++ mobile-app/lib/app/router.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route_annotations.dart';
+import 'package:project_trash/app/route_guards/auth_guard.dart';
 import 'package:project_trash/ui/views/settings/settings_view.dart';
 import 'package:project_trash/ui/views/statistics/statistics_view.dart';
 import 'package:project_trash/ui/views/nfc_links/nfc_links_view.dart';
@@ -10,14 +11,15 @@ import 'package:project_trash/ui/views/trashed_product/trashed_product_view.dart
 
 @MaterialAutoRouter(
   routes: [
-    MaterialRoute(page: StartupView, initial: true),
-    MaterialRoute(page: HomeView),
-    MaterialRoute(page: StatisticsView),
-    MaterialRoute(page: NfcLinksView),
-    MaterialRoute(page: SettingsView),
-    MaterialRoute(page: LoginView),
-    MaterialRoute(page: NfcLinkingView),
-    MaterialRoute(page: TrashedProductView),
+    MaterialRoute(page: StartupView, initial: true, guards: [AuthGuard]),
+    MaterialRoute(page: HomeView, guards: [AuthGuard]),
+    MaterialRoute(page: StatisticsView, guards: [AuthGuard]),
+    MaterialRoute(page: NfcLinksView, guards: [AuthGuard]),
+    MaterialRoute(page: SettingsView, guards: [AuthGuard]),
+    MaterialRoute(page: LoginView, guards: [AuthGuard]),
+    MaterialRoute(page: NfcLinkingView, guards: [AuthGuard]),
+    MaterialRoute(page: TrashedProductView, guards: [AuthGuard]),
+    // MaterialRoute(path: "*", page: UnknownRouteScreen)
   ],
 )
 class $Router {}
